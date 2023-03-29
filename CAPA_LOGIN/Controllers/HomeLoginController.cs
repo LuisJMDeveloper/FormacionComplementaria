7f8f4021fd884fc83918433d19926f57a1f6c5d0
LuisACER <ljarrieta81@misena.edu.co>
2023-03-29 09:02:32 -0500

    Esta parte tiene erro al intentar obtener el data de la tabladata. Eror: mientras la tabla no es responsiva no funciona, cuando los botones estan al mismo nivel de fila resulta undefine

--- CAPA_LOGIN/Controllers/HomeLoginController.cs
+++ CAPA_LOGIN/Controllers/HomeLoginController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
+using CAPA_CORDINADOR.Controllers;
+
+ 
+
 
 namespace CAPA_LOGIN.Controllers
 {
@@ -13,6 +18,11 @@ namespace CAPA_LOGIN.Controllers
             return View();
         }
 
-         
+        public ActionResult CapaCoordinador()
+        {
+            ReporteController reporteController = new ReporteController();
+            return reporteController.IndexReporte();
+        }
+
     }
 }
